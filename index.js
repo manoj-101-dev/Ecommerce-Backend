d548e58b262f2e9eca787f7b287f9324dc77e8d2
manoj-101-dev <manoj19545@gmail.com>
2024-10-28 13:02:14 +0530

    minor changes

--- index.js
+++ index.js
@@ -61,8 +61,8 @@ app.post("/api/create-checkout-session", async (req, res) => {
       payment_method_types: ["card"],
       mode: "payment",
       line_items: lineItems,
-      success_url: `${process.env.FRONTEND_URL}/success`,
-      cancel_url: `${process.env.FRONTEND_URL}/cancel`,
+      success_url: `${process.env.FRONTEND_URL}/success.html`,
+      cancel_url: `${process.env.FRONTEND_URL}/cancel.html`,
     });
 
     res.json({ url: session.url });
